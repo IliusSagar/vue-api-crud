879ca3a6c96e7fbc3001fe5d4f4f7b7b5eb06b33
IliusSagar <sagar01830@gmail.com>
2023-12-15 01:50:27 +0600

    json fake api data fetch testing

---
+++ src/views/List-1.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="about">
+    <h1>List Student</h1>
+
+    <div class="container">
+      <div class="card">
+        <div class="card-header"></div>
+
+        <div class="card-body">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Course</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Created At</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(student, index) in this.students" :key="index">
+                <td>{{ student.id }}</td>
+                <td>{{ student.name }}</td>
+                <td>{{ student.course }}</td>
+                <td>{{ student.email }}</td>
+                <td>{{ student.phone }}</td>
+                <td>{{ student.created_at }}</td>
+                <td>
+                  <RouterLink to="/" class="btn btn-success">Edit</RouterLink>
+                  <button type="button" class="btn btn-danger">Delete</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "students",
+
+  data() {
+    return {
+      students: [],
+    };
+  },
+
+  mounted() {
+    this.getStudents();
+    // console.log('i am here')
+  },
+  methods: {
+    getStudents() {
+      axios.get("http://127.0.0.1:8000/api/students").then((res) => {
+        this.students = res.data.students;
+        console.log(this.students);
+      });
+    },
+  },
+};
+</script>
---
+++ src/views/List.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Data from API</h1>
+    <ul>
+      <!-- <li v-for="item in data" :key="item.id">{{ item.id }}</li> -->
+      <li v-for="item in data" :key="item.id">
+        <img :src="item.image" alt="" style="height: 50px" />
+        <p>{{ item.title }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+const data = ref([]); // Store the fetched data here
+
+const fetchData = () => {
+  // Make a GET request to the API using Axios
+  axios
+    .get("https://fakestoreapi.com/products")
+    .then((response) => {
+      data.value = response.data; // Update the data property with the API response
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+};
+
+onMounted(() => {
+  // Fetch data from the API when the component is created
+  fetchData();
+});
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
